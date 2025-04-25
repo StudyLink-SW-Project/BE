f7af69d597387659cafc37ea7e994eaec2ed3d40
lehojun <hojunchris@naver.com>
2025-04-25 14:58:39 +0900

    [FEAT] #3 일반 로그인 구현

--- src/main/java/com/example/be/repository/UserRepository.java
+++ src/main/java/com/example/be/repository/UserRepository.java
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByProviderId(String providerId);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
