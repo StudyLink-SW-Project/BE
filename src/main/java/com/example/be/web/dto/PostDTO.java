dda6e910b00246c331dc6c02c1f94ee4f4d817a4
lehojun <hojunchris@naver.com>
2025-05-21 11:15:08 +0900

    [FEAT] #21 게시글 목록 조회 기능 구현

--- src/main/java/com/example/be/web/dto/PostDTO.java
+++ src/main/java/com/example/be/web/dto/PostDTO.java
@@ -1,5 +1,6 @@
 package com.example.be.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -25,17 +27,27 @@ public class PostDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class postResponseDTO {
+        Long id;
         String title;
         String content;
-        String user;
-        LocalDate date;
+        String userName;
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate createDate;
+        int commentCount;
+        boolean isDone;
     }
 
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class postListResponseDTO{
-        List<postResponseDTO> postList;
+    @Schema(description = "게시글 페이지네이션 응답 DTO")
+    public static class PageResponseDTO {
+        private List<postResponseDTO> posts;
+        private int currentPage;
+        private int totalPages;
+        private long totalElements;
+        private boolean first;
+        private boolean last;
     }
 }
