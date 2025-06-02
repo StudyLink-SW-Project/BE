749f1692b1e60e8c2f1e02ecfbfe6b35e556e192
lehojun <hojunchris@naver.com>
2025-06-02 17:18:15 +0900

    [FEAT] #37 댓글 삭제 기능 구현

--- src/main/java/com/example/be/repository/CommentRepository.java
+++ src/main/java/com/example/be/repository/CommentRepository.java
@@ -4,6 +4,9 @@ import com.example.be.domain.Comment;
 import com.example.be.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    Optional<Comment> findByIdAndUserId(Long id, Long userId);
 }
